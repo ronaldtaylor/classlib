a0ed2640258b6b83d194006024419ff596e9a6ad
ronaldtaylor <ronaldbtaylor@comcast.net>
2014-10-05 10:06:30 -0600

    add project

--- ClassLibrary1/project.cs
+++ ClassLibrary1/project.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
-    public class Class1
+    public class Project
     {
+        public Project()
+        {
+            Debug.WriteLine("");
+        }
     }
 }
